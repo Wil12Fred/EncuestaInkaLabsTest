5186902e8c7647fe166112780ce096e06bafc2fc
Wilber Cutire <wilbert_030@icloud.com>
2019-10-28 19:10:22 -0500

    Add url to report

--- backend/encuesta/views.py
+++ backend/encuesta/views.py
@@ -53,15 +53,15 @@ def reporte_alternativa_orden(encuesta_pregunta, request):
 			reporte[alternativa_url]['total']=0
 		reporte[alternativa_url][alternativa_orden.orden]=alternativa_orden.total
 		reporte[alternativa_url]['total'] += alternativa_orden.total
-	for k1,v1 in reporte.items():
+	"""for k1,v1 in reporte.items():
 		total = v1['total']
 		for k2,v2 in v1.items():
 			if(k2!='total'):
 				reporte[k1][k2]=v2/total
-		"""if (k!='total'):
-			reporte[k]=v/
-			for alternativa_orden in alternativas_orden:
-				alternativaserializer = AlternativaSerializer(alternativa_orden.alternativa, context={'request': request})
+		if (k!='total'):
+			reporte[k]=v/"""
+	"""for alternativa_orden in alternativas_orden:
+		alternativaserializer = AlternativaSerializer(alternativa_orden.alternativa, context={'request': request})
 		alternativa_url = alternativaserializer.data['url']
 		reporte[alternativa_url][alternativa_orden.orden]=alternativa_orden.total/reporte[alternativa_url]['total']"""
 	return reporte
